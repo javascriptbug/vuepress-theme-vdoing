4cb2adde2c31d3df181b50b53d16db8fba8617cf
hangingpot <chy282811830@outlook.com>
2021-04-26 18:44:40 +0800

    commit munit

--- munit/src/index.js
+++ munit/src/index.js
@@ -1,25 +1,28 @@
 /*
-* @author chenhaiyong
-* Date: 18/12/25
-*/
+ * @author chenhaiyong
+ * Date: 18/12/25
+ */
 
 
 import MTitle from '../packages/m-title/index'
 import MCascader from '../packages/m-cascader/index'
+import MCascaderPanel from '../packages/m-cascader-panel/index'
 const components = [
- 
-  MTitle,
-  MCascader
+
+	MTitle,
+	MCascader,
+	MCascaderPanel
 ]
 const install = function(Vue) {
-  if(install.installed) return
-  components.map(component => Vue.component(component.name, component))
+	if (install.installed) return
+	components.map(component => Vue.component(component.name, component))
 }
 
 
 export default {
-  install,
+	install,
 
-  MTitle,
-  MCascader
+	MTitle,
+	MCascader,
+	MCascaderPanel
 }
